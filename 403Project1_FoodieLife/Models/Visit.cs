fd1bb07f71707c072bb11d4dd4fc4219125674ff
Cherileigh Leavitt <cleavit6@byu.edu>
2019-11-19 20:11:20 -0700

    added views and edited visit model

--- 403Project1_FoodieLife/Models/Visit.cs
+++ 403Project1_FoodieLife/Models/Visit.cs
@@ -9,15 +9,26 @@ namespace _403Project1_FoodieLife.Models
     public class Visit
     {
         [Required]
-        public static int restID { get; set; }
+        public static int visitID { get; set; }
 
-        [Required(ErrorMessage = "Please enter a restaurant name.")]
-        [MaxLength(50, ErrorMessage = "50 character limit.")]
-        [Display(Name = "Restaurant Name")]
+        [Required(ErrorMessage = "Please choose a restaurant.")]
+        [Display(Name = "Restaurant")]
         public static string restName { get; set; }
 
-        [Required(ErrorMessage = "Please select a category.")]
-        [Display(Name = "Restaurant Category")]
-        public static string restCategory { get; set; }
+        [Required(ErrorMessage = "Please rate your visit."]
+        [Display(Name = "Rating")]
+        [Range(1,5)]
+        public int visitRating { get; set; }
+
+        [Required(ErrorMessage = "Please enter a location.")]
+        [Display(Name = "Location")]
+        public static string restLocation { get; set; }
+
+        [Required]
+        [Display(Name = "What did you order?")]
+        public static string order { get; set; }
+
+        [Display(Name = "Comments")]
+        public string comments { get; set; }
     }
 }
